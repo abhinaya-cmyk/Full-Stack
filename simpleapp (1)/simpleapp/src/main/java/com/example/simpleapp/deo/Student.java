fe45e709fbe502d962e275239eea851303834160
Abinaya <abhiizaraa@gmail.com>
2026-03-18 11:33:03 +0530

    Add files via upload

---
+++ simpleapp (1)/simpleapp/src/main/java/com/example/simpleapp/deo/Student.java
@@ -0,0 +1,16 @@
+package com.example.simpleapp.deo;
+
+import lombok.AllArgsConstructor;
+import lombok.Generated;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+//* Student POJO to store data
+public class Student {
+    int id;
+    String firstName;
+    String lastName;
+}
